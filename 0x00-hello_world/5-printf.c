5b0bd634e6356d6925daeafa439b25ccfd560738
ricozmwas <mwangi686@gmail.com>
2023-07-13 04:42:32 -0700

    5-printf.c executable

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program printing a line with printf
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n"
+return (0);
+}
+
