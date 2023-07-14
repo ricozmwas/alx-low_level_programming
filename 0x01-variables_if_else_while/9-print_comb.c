59502aa8ffd0b1838a7759ceae6b0bf99c9f9d60
ricozmwas <mwangi686@gmail.com>
2023-07-14 14:53:41 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print single digit numbers
+ *
+ * Return: 0(success)
+ */
+int main(void)
+
+{
+	int number;
+
+	for (number = 0; number < 10; ++number)
+	{
+		putchar(number + '0');
+	}
+	if (number == 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
