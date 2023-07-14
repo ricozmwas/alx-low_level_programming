ebc54eb630a027db8774d339dd0d62c1bbd8857c
ricozmwas <mwangi686@gmail.com>
2023-07-14 14:19:39 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print numbers of base 16
+ * and n lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+
+{
+	char alphabet;
+
+	int number;
+
+	for (number = 0; number < 16; ++number)
+	{
+		putchar((number) + '0');
+	}
+	for (alphabet = 'a'; alphabet = 'f'; ++alphabet)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
