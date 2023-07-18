a2bf877896a9a63cecd47a51eab65e487829a648
ricozmwas <mwangi686@gmail.com>
2023-07-18 05:50:54 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * starting from 00:00 to 23:59
+ *
+ */
+void jack_bauer(void)
+{
+	int b, j;
+
+	b = 0;
+
+	while (b < 24)
+	{
+		j = 0;
+
+		while (j < 60)
+		{
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		b++;
+	}
+}
