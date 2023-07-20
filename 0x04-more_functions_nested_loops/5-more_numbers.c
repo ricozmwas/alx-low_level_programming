b1254c76e1b7a4bb3c79c31f3a7b165d00872f66
ricozmwas <mwangi686@gmail.com>
2023-07-20 13:43:29 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,18 +9,18 @@
  */
 void more_numbers(void)
 {
-    int a, b;
+	int a, b;
 
-    for (a = 0; a < 10; ++a)
-    {
-        for (b = 0; b <= 14; ++b)
-        {
-            if (b >= 10)
-                _putchar(b / 10 + '0');
-
-            _putchar(b % 10 + '0');
-        }
-
-        _putchar('\n');
-    }
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b / 10 != 0)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
