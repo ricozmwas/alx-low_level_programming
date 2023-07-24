2f8f681ecb1a8493f70708215ca82fe6bda611cf
ricozmwas <mwangi686@gmail.com>
2023-07-24 05:40:48 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string, in reverse
+  * @s: string parameter
+  *
+  */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
