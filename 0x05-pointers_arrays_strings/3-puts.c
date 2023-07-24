0640da1ee5c5971244de6ff927f707c5a40dccdd
ricozmwas <mwangi686@gmail.com>
2023-07-24 04:05:11 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _puts - prints a string, followed by a new line
+  *
+  * @str: string parameter
+  *
+  */
+
+void _puts(char *str)
+
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
